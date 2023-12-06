65745e6b01ae8f769405287b06eab5345319e262
pithieLS <59534016+pithieLS@users.noreply.github.com>
2023-12-06 19:25:46 +0100

    Gamemode (c++->bp) + Player controller (c++->bp) + level

---
+++ Source/GameJam_Anatole/Public/Core/AFA_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AFA_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM_ANATOLE_API AAFA_GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GameJam_Anatole/Public/Core/AFA_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AFA_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM_ANATOLE_API AAFA_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
